c98affdfae2593e4d8796957e7f143ae0648e6be
Antonio Rus <rus.antonio@gmail.com>
2013-07-06 09:37:47 +0000

    added comments to code

--- web.js
+++ web.js
@@ -1,11 +1,11 @@
 var express = require('express');
-var fs = require('fs');
+var fs = require('fs'); //http://nodejs.org/api/fs.html#fs_file_system
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buffer = fs.readFileSync('index.html');
-  var output = buffer.toString();
+  var buffer = fs.readFileSync('index.html'); //http://nodejs.org/api/fs.html#fs_fs_readfilesync_filename_options
+  var output = buffer.toString();  //http://nodejs.org/api/buffer.html#buffer_buf_tostring_encoding_start_end 
   response.send(output);
 });
 
